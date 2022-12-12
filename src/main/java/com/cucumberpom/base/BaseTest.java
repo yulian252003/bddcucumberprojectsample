47562f6f07f0f11abe9fb9192d01ade9ce6e38a7
yulian252003 <yulian252003@hotmail.com>
2022-12-12 11:01:16 -0500

    20update

--- src/main/java/com/cucumberpom/base/BaseTest.java
+++ src/main/java/com/cucumberpom/base/BaseTest.java
@@ -36,8 +36,8 @@ public class BaseTest
 		{
 			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 			ChromeOptions options = new ChromeOptions();
-			  options.addArguments("--headless");
-			  options.addArguments("window-size=1920x1080");
+			  options.addArguments("headless");
+			  options.addArguments("window-size=1920,1080");
 			  options.addArguments("no-sandbox");
 			  options.addArguments("–disable-dev-shm-usage");
 			  options.addArguments("start-maximized");
