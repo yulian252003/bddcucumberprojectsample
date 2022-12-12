6938ee366d75af4aa8877c12931a87e6682b641c
yulian252003 <yulian252003@hotmail.com>
2022-12-12 09:07:06 -0500

    16update

--- src/test/java/com/cucumberpom/runner/TestRunner.java
+++ src/test/java/com/cucumberpom/runner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		         features = "src/test/java/com/cucumber/features/Application.feature",
                  glue= {"com/cucumber/stepdefinitions"},
-                 plugin = {"pretty", "html:target/cucumber.html"},
+                 plugin = {"pretty", "json:target/cucumber-report.json"},
 		         dryRun = false,
 		         monochrome = true,
 		         strict = true
