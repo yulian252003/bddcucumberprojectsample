c4d82787cfdf3dc5d3493923f2342fd2a5202f4b
yulian252003 <yulian252003@hotmail.com>
2022-12-13 17:39:45 -0500

    30updatess

--- src/test/java/com/cucumberpom/runner/TestRunner.java
+++ src/test/java/com/cucumberpom/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		         features = "src/test/java/com/cucumber/features/Application.feature",
+		         features = "src/test/java/com/cucumber/features",
                  glue= {"com/cucumber/stepdefinitions"},
                  plugin = {"pretty", "html:target/cucumber.html"},
 		         dryRun = false,
